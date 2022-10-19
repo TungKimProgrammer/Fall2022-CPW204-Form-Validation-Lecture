84515519ad6a378f8c6591cb14f93cc9cc2ba188
TUNG N KIM <Tungkims@gmail.com>
2022-10-18 22:48:36 -0700

    added function IsPresent to validate txtbx data

--- js/main.js
+++ js/main.js
@@ -3,4 +3,16 @@ window.onload = function () {
     formBtn.onclick = main;
 };
 function main() {
+    if (isPresent("first-name", "First name is required"))
+        isPresent("last-name", "Last name is required");
+}
+function isPresent(id, errMsg) {
+    var inputBox = document.getElementById(id);
+    var inputValue = inputBox.value;
+    if (inputValue == "") {
+        var errSpan = inputBox.nextElementSibling;
+        errSpan.innerText = errMsg;
+        return false;
+    }
+    return true;
 }
--- ts/main.ts
+++ ts/main.ts
@@ -4,5 +4,23 @@ window.onload = function(){
 }
 
 function main():void {
-    
+    if (isPresent("first-name", "First name is required"))
+    isPresent("last-name", "Last name is required")
+}
+
+/**
+ * returns true if the textbox with the given id has some text inside
+ * @param id the id of the <input type="text"> to validate
+ * @param errMsg message to display in the sibling span of the textbox
+ * @returns 
+ */
+function isPresent(id: string, errMsg: string):boolean {
+    let inputBox = <HTMLInputElement>document.getElementById(id);
+    let inputValue = inputBox.value;
+    if (inputValue == "") {
+        let errSpan = <HTMLElement>inputBox.nextElementSibling;
+        errSpan.innerText = errMsg;
+        return false;
+    }
+    return true;
 }
