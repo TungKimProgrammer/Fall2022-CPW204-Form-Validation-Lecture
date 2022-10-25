50ab495c0169196f7408aa3dfacf9b06ae4d0254
TUNG N KIM <Tungkims@gmail.com>
2022-10-25 16:36:16 -0700

    practice DOM

--- ts/main.ts
+++ ts/main.ts
@@ -3,7 +3,31 @@ window.onload = function(){
     formBtn.onclick = main;
 }
 
+/**
+ * change the message heading color every time it's clicked
+ */
+function changeHeading(){
+    let heading = <HTMLElement>this;
+    let red = Math.floor(Math.random() * 255 + 1);
+    let green = Math.floor(Math.random() * 255 + 1);
+    let blue = Math.floor(Math.random() * 255 + 1);
+    let color = "rgb(" + red + "," + green + "," + blue +")";
+    heading.style.color = color;
+}
+
 function main():void {
+    let msgHeading = document.createElement("h2");
+    msgHeading.innerText = "Processing Form";
+    msgHeading.setAttribute("class", "message");
+    msgHeading.onclick = changeHeading;
+
+    let h1 = document.querySelector("h1");
+    h1.insertAdjacentElement("afterend", msgHeading);
+
+    setTimeout(function(){
+        msgHeading.remove();
+    }, 3000)
+
     resetErrMessage();
     isPresent("first-name", "First name is required");
     isPresent("last-name", "Last name is required");
@@ -12,6 +36,9 @@ function main():void {
     checkValidDate();
 }
 
+/**
+ * 
+ */
 function checkValidDate() {
     let dobBox = <HTMLInputElement>document.getElementById("dob");
     let dob = dobBox.value;
@@ -38,6 +65,11 @@ function isPresent(id: string, errMsg: string):boolean {
     return true;
 }
 
+/**
+ * 
+ * @param input 
+ * @returns 
+ */
 function isValidDate(input: string):boolean{
     // Validating mm/dd/yyyy or m/d/yyyy
     // \d{1,2}\/d{1,2}\/d{4}
